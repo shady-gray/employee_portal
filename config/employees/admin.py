6a03611f3550b35ea64ba8a72fb6c6ca700558ef
basil-bassey <akpanbasilbassey@gmail.com>
2026-08-05 23:14:57 +0100

    Created employees Model

--- config/employees/admin.py
+++ config/employees/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Employee
 
 # Register your models here.
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = (
+        'employee_id',
+        'full_name',
+        'department',
+        'email',
+        'hire_date',
+    )
--- config/employees/models.py
+++ config/employees/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    employee_id = models.AutoField(primary_key=True)
+    full_name = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    email = models.EmailField()
+    hire_date = models.DateField()
+
+    def __str__(self):
+        return self.full_name
